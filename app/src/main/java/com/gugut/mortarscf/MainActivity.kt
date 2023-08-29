679fc4ba02b8d00c1949aa262d68313d92de3b4b
gugut-net <69830996+gugut-net@users.noreply.github.com>
2023-08-28 23:37:30 -0400

    hilt added.

--- app/src/main/java/com/gugut/mortarscf/MainActivity.kt
+++ app/src/main/java/com/gugut/mortarscf/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.gugut.mortarscf.components.MainScreen
 import com.gugut.mortarscf.drawer.Drawer
 import com.gugut.mortarscf.ui.theme.MortarsCFTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
